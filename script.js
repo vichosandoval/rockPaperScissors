341759dc7e1df63991613baf8207e844c6af621f
vichosandoval <vichosandoval@DESKTOP-I0KM50S>
2023-05-25 14:01:34 -0300

    Adding the condicional of victory or tie in the game

--- script.js
+++ script.js
@@ -1,4 +1,40 @@
-let playerChoise;
-let machineChoice;
+let playerChoise = window.prompt('Enter your input:');
+let choices = ["Rock", "Papper", "Scissors"];
+let machineChoice = 0;
 let playerScore = 0;
 let machineScore = 0;
+
+while (playerScore < 5 || machineScore < 5){
+    let randomMove = Math.floor(Math.random() * 2);
+    console.log("Choose your move:", playerChoise);
+    machineChoice = choices[randomMove];
+    if(playerChoise == "rock" && machineChoice == "Scissors"){
+        playerScore++;
+        console.log("Point for the player!");
+        console.log(`Player Score: ${playerScore} / Machine Score: ${machineScore}`);
+    } else if(machineChoice == "rock" && playerChoise == "Scissors"){
+        machineScore++;
+        console.log("Point for the Machine!");
+        console.log(`Player Score: ${playerScore} / Machine Score: ${machineScore}`);
+    } else if(playerChoise == "Sissors" && machineChoice == "Papper"){
+        playerScore++;
+        console.log("Point for the player!");
+        console.log(`Player Score: ${playerScore} / Machine Score: ${machineScore}`);
+    } else if(machineChoice == "Sissors" && playerChoise == "Papper"){
+        machineChoice++;
+        console.log("Point for the Machine!");
+        console.log(`Player Score: ${playerScore} / Machine Score: ${machineScore}`);
+    }else if(playerChoise == "Papper" && machineChoice == "Rock"){
+        playerScore++;
+        console.log("Point for the player!");
+        console.log(`Player Score: ${playerScore} / Machine Score: ${machineScore}`);
+    } else if(machineChoice == "Papper" && playerChoise == "Rock"){
+        machineChoice++;
+        console.log("Point for the Machine!");
+        console.log(`Player Score: ${playerScore} / Machine Score: ${machineScore}`);
+    } else {
+        console.log("Tie!");
+        console.log(`Player Score: ${playerScore} / Machine Score: ${machineScore}`);
+    }
+    
+}
