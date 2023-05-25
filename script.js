5974108cdd21212d18863b23517c3f4ce668673a
vichosandoval <vichosandoval@DESKTOP-I0KM50S>
2023-05-25 14:19:12 -0300

    adding conditional to five wins ends the game

--- script.js
+++ script.js
@@ -4,37 +4,44 @@ let machineChoice = 0;
 let playerScore = 0;
 let machineScore = 0;
 
-while (playerScore < 5 || machineScore < 5){
-    let randomMove = Math.floor(Math.random() * 2);
-    console.log("Choose your move:", playerChoise);
-    machineChoice = choices[randomMove];
-    if(playerChoise == "rock" && machineChoice == "Scissors"){
-        playerScore++;
-        console.log("Point for the player!");
+while (playerScore <= 5 || machineScore <= 5){
+    if (playerScore < 5 || machineScore < 5){
+        let randomMove = Math.floor(Math.random() * 2);
+        console.log("Choose your move:", playerChoise);
+        machineChoice = choices[randomMove];
+        if(playerChoise == "rock" && machineChoice == "Scissors"){
+            playerScore++;
+            console.log("Point for the player!");
+            console.log(`Player Score: ${playerScore} / Machine Score: ${machineScore}`);
+        } else if(machineChoice == "rock" && playerChoise == "Scissors"){
+            machineScore++;
+            console.log("Point for the Machine!");
+            console.log(`Player Score: ${playerScore} / Machine Score: ${machineScore}`);
+        } else if(playerChoise == "Sissors" && machineChoice == "Papper"){
+            playerScore++;
+            console.log("Point for the player!");
+            console.log(`Player Score: ${playerScore} / Machine Score: ${machineScore}`);
+        } else if(machineChoice == "Sissors" && playerChoise == "Papper"){
+            machineChoice++;
+            console.log("Point for the Machine!");
+            console.log(`Player Score: ${playerScore} / Machine Score: ${machineScore}`);
+        }else if(playerChoise == "Papper" && machineChoice == "Rock"){
+            playerScore++;
+            console.log("Point for the player!");
+            console.log(`Player Score: ${playerScore} / Machine Score: ${machineScore}`);
+        } else if(machineChoice == "Papper" && playerChoise == "Rock"){
+            machineChoice++;
+            console.log("Point for the Machine!");
+            console.log(`Player Score: ${playerScore} / Machine Score: ${machineScore}`);
+        } else {
+            console.log("Tie!");
+            console.log(`Player Score: ${playerScore} / Machine Score: ${machineScore}`);
+        }
+    } else if (playerScore == 5){
+        console.log("PLAYER WINS!");
         console.log(`Player Score: ${playerScore} / Machine Score: ${machineScore}`);
-    } else if(machineChoice == "rock" && playerChoise == "Scissors"){
-        machineScore++;
-        console.log("Point for the Machine!");
-        console.log(`Player Score: ${playerScore} / Machine Score: ${machineScore}`);
-    } else if(playerChoise == "Sissors" && machineChoice == "Papper"){
-        playerScore++;
-        console.log("Point for the player!");
-        console.log(`Player Score: ${playerScore} / Machine Score: ${machineScore}`);
-    } else if(machineChoice == "Sissors" && playerChoise == "Papper"){
-        machineChoice++;
-        console.log("Point for the Machine!");
-        console.log(`Player Score: ${playerScore} / Machine Score: ${machineScore}`);
-    }else if(playerChoise == "Papper" && machineChoice == "Rock"){
-        playerScore++;
-        console.log("Point for the player!");
-        console.log(`Player Score: ${playerScore} / Machine Score: ${machineScore}`);
-    } else if(machineChoice == "Papper" && playerChoise == "Rock"){
-        machineChoice++;
-        console.log("Point for the Machine!");
-        console.log(`Player Score: ${playerScore} / Machine Score: ${machineScore}`);
-    } else {
-        console.log("Tie!");
+    }else if (machineScore == 5){
+        console.log("MACHINE WINS!");
         console.log(`Player Score: ${playerScore} / Machine Score: ${machineScore}`);
     }
-    
 }
