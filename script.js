c84fefa26eb4afe94d2d26c6c887751bc3f9b2a0
vichosandoval <vichosandoval@DESKTOP-I0KM50S>
2023-05-27 09:05:03 -0300

    Adding the string showing the move that evryone has make and a message annoucing the winner

--- script.js
+++ script.js
@@ -5,23 +5,33 @@ let choices = ["rock", "papper", "scissors"];
 
 while(stillPlaying){
     if (playerScore === 5 || machineScore === 5){
+        if(playerScore === 5){
+            console.log("PLAYER WINS THE GAME!");
+            console.log(`Player score: ${playerScore} || Machine score: ${machineScore}`);
+        }else{
+            console.log("MACHINE WINS THE GAME!");
+            console.log(`Player score: ${playerScore} || Machine score: ${machineScore}`);
+        }
         stillPlaying = false;
     } else {
-        let playerChoise = window.prompt('Enter your input:')
-        playerChoise = playerChoise.toLowerCase();;
+        let playerChoise = window.prompt('Enter your input:');
+        playerChoise = playerChoise.toLowerCase();
         let randomPlay = Math.floor(Math.random() * 3);
         let machineChoise = choices[randomPlay];
         if ((playerChoise == "rock" && machineChoise == "scissors") || 
             (playerChoise == "papper" && machineChoise == "rock") || 
             (playerChoise == "scissors" && machineChoise == "papper")){
                 playerScore++;
+                console.log(`Player use: ${playerChoise} || Machine use: ${machineChoise}`);
                 console.log("Player Wins!");
                 console.log(`Player score: ${playerScore} || Machine score: ${machineScore}`);
         } else if (playerChoise === machineChoise){
-            console.log("TIE!")
+            console.log(`Player use: ${playerChoise} || Machine use: ${machineChoise}`);
+            console.log("TIE!");
             console.log(`Player score: ${playerScore} || Machine score: ${machineScore}`);
         } else {
             machineScore++
+            console.log(`Player use: ${playerChoise} || Machine use: ${machineChoise}`);
             console.log("Machine Wins!");
             console.log(`Player score: ${playerScore} || Machine score: ${machineScore}`);
         }
