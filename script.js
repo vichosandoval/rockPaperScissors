309f22c69e78bc627d0003545c3b6f2314750fb8
vichosandoval <vichosandoval@DESKTOP-I0KM50S>
2023-05-26 22:25:02 -0300

    Changed the logic of the game

--- script.js
+++ script.js
@@ -4,13 +4,27 @@ let stillPlaying = true;
 let choices = ["rock", "papper", "scissors"];
 
 while(stillPlaying != true){
-    let playerChoise = window.prompt('Enter your input:');
-    let randomPlay = numeroAleatorio = Math.floor(Math.random() * 3);
-    let machineChoise = choices[randomPlay];
     if (playerScore = 5 || machineScore == 5){
         stillPlaying = false;
     } else {
-        
+        let playerChoise = window.prompt('Enter your input:')
+        playerChoise = playerChoise.toLowerCase();;
+        let randomPlay = numeroAleatorio = Math.floor(Math.random() * 3);
+        let machineChoise = choices[randomPlay];
+        if ((playerChoise == "rock" && machineChoise == "scissors") || 
+            (playerChoise == "papper" && machineChoise == "rock") || 
+            (playerChoise == "scissors" && machineChoise == "papper")){
+                playerScore++;
+                console.log("Player Wins!");
+                console.log(`Player score: ${playerScore} || Machine score: ${machineScore}`);
+        } else if (playerChoise === machineChoise){
+            console.log("TIE!")
+            console.log(`Player score: ${playerScore} || Machine score: ${machineScore}`);
+        } else {
+            machineScore++
+            console.log("Machine Wins!");
+            console.log(`Player score: ${playerScore} || Machine score: ${machineScore}`);
+        }
     }
 }
 
